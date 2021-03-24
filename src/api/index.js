97d9d89ab01e3fe635c24f6edc190cf3cee4ac41
nnd1402 <nenadmihajlovic1402@gmail.com>
2021-03-24 16:54:09 +0100

    Country picker and bar chart created

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,16 @@ import axios from 'axios';
 
 const url = 'https://covid19.mathdro.id/api';
 
-export const fetchData = async () => {
+export const fetchData = async (country) => {
+    let changeableUrl = url;
+
+    if(country) {
+        changeableUrl = `${url}/countries/${country}`
+    }
+
+
     try {
-        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(changeableUrl);
 
         const modifiedData = {
             confirmed,
@@ -16,7 +23,7 @@ export const fetchData = async () => {
         return modifiedData;
         
     } catch (error) {
- 
+        console.log(error)
     }
 }
 
@@ -32,6 +39,16 @@ export const fetchDailyData = async() => {
 
         return modifiedData;
     } catch (error) {
+        console.log(error)
+    }
+}
 
+export const fetchCountries = async () => {
+    try {
+        const {data: {countries}} = await axios.get(`${url}/countries`);
+
+        return countries.map((country) => country.name);
+    } catch (error) {
+        console.log(error)
     }
 }
